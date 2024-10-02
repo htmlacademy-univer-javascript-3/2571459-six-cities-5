bf5174d105df0beb8244f7b1482a009a1b5085ae
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-10-03 00:18:25 +0500

    Промежуточные

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {Header} from './Components/Header.tsx';
+import {Main} from './Components/Main.tsx';
+
+export function App() {
+  return (
+    <div className='page page--gray page--main'>
+      <Header/>
+      <Main/>
+    </div>
+  );
+}
