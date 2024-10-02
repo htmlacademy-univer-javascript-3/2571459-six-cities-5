cc042d91f450bce49cb9739267c52ef7146277f2
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-10-03 01:01:42 +0500

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import {Header} from './Components/Header.tsx';
 import {Main} from './Components/Main.tsx';
 
-export function App() {
+interface AppProps {
+  backgroundColor: string;
+  placesToStayCount: number;
+}
+
+export function App(props: AppProps) {
   return (
-    <div className='page page--gray page--main'>
+    <div className='page page--main' style={{
+      backgroundColor: props.backgroundColor
+    }}
+    >
       <Header/>
-      <Main/>
+      <Main placesToStayCount={props.placesToStayCount}/>
     </div>
   );
 }
