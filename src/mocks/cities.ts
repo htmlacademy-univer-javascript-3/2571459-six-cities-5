9cc18954ce4472ac81e16e55431c3ba98923c5d9
Stepan_Kuchin <stepa.ku4in@gmail.com>
2024-12-08 21:01:56 +0500

    докинул координаты в моки

--- src/mocks/cities.ts
+++ src/mocks/cities.ts
@@ -8,23 +8,23 @@ export const cities: City[] = [
   },
   {
     name: 'Brussels',
-    lat: 52.370228,
-    lng: 4.902137,
+    lat: 50.8504,
+    lng: 4.34878,
   },
   {
     name: 'Cologne',
-    lat: 52.370228,
-    lng: 4.902137,
+    lat: 50.9333,
+    lng: 6.95,
   },
   {
     name: 'Dusseldorf',
-    lat: 52.370228,
-    lng: 4.902137,
+    lat: 51.2217,
+    lng: 6.77616,
   },
   {
     name: 'Hamburg',
-    lat: 52.370228,
-    lng: 4.902137,
+    lat: 53.5753,
+    lng: 10.0153,
   },
   {
     name: 'Paris',
